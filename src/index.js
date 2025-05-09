7a2935438ab32245da943a0b99f3c0e9c0e7668c
Abhilash67 <149377226+Abhilash67@users.noreply.github.com>
2025-05-09 23:06:18 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Main entry point for the universal-sdk package
+import { createAuthProvider } from './auth-provider.js';
+
+// Export the factory function to create auth providers
+export default {
+  createAuthProvider
+};
